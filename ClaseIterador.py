1c67fc2a3db190d43d1de0112fa1a20bece3b0e1
Hector Badillo <dsm.badillo@gmail.com>
2023-10-11 21:32:23 -0600

    Clase iterador

--- ClaseIterador.py
+++ ClaseIterador.py
@@ -15,3 +15,24 @@ class MiIterador:
 mi_iterador = MiIterador(5)
 for elemento in mi_iterador:
     print(elemento)
+
+class MiIterador:
+    def __init__(self, limite):
+        self.limite = limite
+        self.valor = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.valor < self.limite:
+            resultado = self.valor
+            self.valor += 1
+            return resultado
+        else:
+            raise StopIteration
+
+# Usar la clase iteradora en un bucle for
+mi_iterador = MiIterador(5)
+for elemento in mi_iterador:
+    print(elemento)
